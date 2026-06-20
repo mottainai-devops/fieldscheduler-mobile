c2e7fedb6b160975e707794e9076b3277c4ca5e6
Field Scheduler DevOps <ci@fieldscheduler.net>
2026-06-20 12:05:54 -0400

    fix(mobile): add go_router import to route_detail_screen (CI fix)

--- lib/screens/route_detail_screen.dart
+++ lib/screens/route_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:provider/provider.dart';
